f734f07c8d2a8b2d597f95a130d4f1a91342ccaf
Norberto <norbertoj54@hotmail.com>
2025-03-22 00:31:51 -0300

    Add adapter to Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://njl-prueba-astro.netlify.app/",
-  integrations: [preact()]
+  integrations: [preact()],
+  adapter: netlify()
 });
