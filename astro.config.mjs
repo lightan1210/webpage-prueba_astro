4d9d7c6e8588349f66341d6e569045ad1601983b
Norberto <norbertoj54@hotmail.com>
2025-03-22 01:08:30 -0300

    Fix problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://njl-prueba-astro.netlify.app/",
-  integrations: [preact()],
-  adapter: netlify()
+  integrations: [preact()]
 });
